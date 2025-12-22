49a0677f8905c6679289e1914356bb46fb5b4eb2
polyprogrammist <vad.e.volodin@gmail.com>
2025-12-23 01:52:38 +0200

    correct bot name

--- src/lib/links.ts
+++ src/lib/links.ts
@@ -9,6 +9,6 @@ export const TELEGRAM_COMMUNITY = {
   uk: "",
 };
 
-export const BOT_LINK = "https://t.me/minddy_io_bot";
+export const BOT_LINK = "https://t.me/minddy_bot";
 
 export const WHITEPAPER_LINK = "/whitepaper.pdf";
